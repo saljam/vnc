f43f5f8df23477a1569812cd58ca180902f72743
Salman Aljammaz <aljamms8@cs.man.ac.uk>
2013-01-10 12:07:16 +0000

    cleanup dependencies

--- chrome-app/vnc.js
+++ chrome-app/vnc.js
@@ -1,45 +1,45 @@
-var rfb;
-
-function updateState(rfb, state, oldstate, msg) {
-	switch (state) {
-	case 'failed':
-	case 'fatal':
-		$("#form-connect").slideDown();
-		$('<div class="alert">' +
-			'<button type="button" class="close" data-dismiss="alert">&#215;</button>' + 
- 			'<strong>Oops!</strong> ' + msg + '</div>').prependTo("#form-connect");
-		break;
-	case 'normal':
-	case 'disconnected':
-	case 'loaded':
-	default:
-	}
-}
-
-function dial(host, port) {
-	var password;
-
-	document.title = host;
-
-	pwd = "";
-
-	rfb = new RFB({
-		'target': $D('screen'),
-		'encrypt': false,
-		'repeaterID': '',
-		'true_color': true,
-		'local_cursor': true,
-		'shared': true,
-		'view_only': false,
-		'updateState': updateState,
-		'onPasswordRequired': function() {
-			$("#form-pwd").slideDown();
-		}
-	});
-	rfb.connect(host, port, pwd, '');
-}
-
 $(function () {
+	var rfb;
+
+	var updateState = function (rfb, state, oldstate, msg) {
+		switch (state) {
+		case 'failed':
+		case 'fatal':
+			$("#form-connect").slideDown();
+			$('<div class="alert">' +
+				'<button type="button" class="close" data-dismiss="alert">&#215;</button>' + 
+ 				'<strong>Oops!</strong> ' + msg + '</div>').prependTo("#form-connect");
+			break;
+		case 'normal':
+		case 'disconnected':
+		case 'loaded':
+		default:
+		}
+	}
+
+	var dial = function(host, port) {
+		var password;
+
+		document.title = host;
+
+		pwd = "";
+
+		rfb = new RFB({
+			'target': $D('screen'),
+			'encrypt': false,
+			'repeaterID': '',
+			'true_color': true,
+			'local_cursor': true,
+			'shared': true,
+			'view_only': false,
+			'updateState': updateState,
+			'onPasswordRequired': function() {
+				$("#form-pwd").slideDown();
+			}
+		});
+		rfb.connect(host, port, pwd, '');
+	}
+
 	$("#form-pwd").hide().submit(function(){
 		rfb.sendPassword(password.value);
 		$("#form-pwd").slideUp();
